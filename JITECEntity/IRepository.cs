c8d7e345c4cbf00004d52a85c5a09497c63ab9fd
koudenpa <koudenpa@hotmail.com>
2022-04-07 20:49:58 +0900

    つぶやき状態とRepoのインタフェースの定義

---
+++ JITECEntity/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+
+namespace JITECEntity
+{
+    public interface IRepository
+    {
+        Task<ExamPart> GetExamPartAsync(string examId, string examPartId);
+        Task SaveExamPartAsync(ExamPart examPart);
+
+        Task<ExamPartOnTwitter> GetExamPartOnTwitterAsync(string examId, string examPartId);
+        Task SaveExamPartAsync(ExamPartOnTwitter examPart);
+    }
+}
