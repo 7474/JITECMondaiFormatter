85a32c89b521c70e12cdfd6f82ff8bfac9cd6f78
koudenpa <koudenpa@hotmail.com>
2022-04-09 01:55:41 +0900

    ランダムに問題を選択する

--- JITECKakomonFunctions/TweetNewKakomon.cs
+++ JITECKakomonFunctions/TweetNewKakomon.cs
@@ -1,5 +1,6 @@
 using Azure.Storage.Blobs;
 using JITECEntity;
+using Microsoft.AspNetCore.WebUtilities;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Configuration;
@@ -29,6 +30,60 @@ namespace JITECKakomonFunctions
             string containerName = config.GetValue<string>("BLOB_CONTAINER_NAME");
             var blobClient = new BlobServiceClient(storageConnectionString);
             _repository = new Repository(blobClient, containerName);
+        }
+
+        public record ListKakomonResponse(IList<ExamPartSummary> Exams) { }
+
+        [Function("ListKakomon")]
+        public async Task<HttpResponseData> RunListKakomonAsync(
+            [HttpTrigger(AuthorizationLevel.Function, "get")] HttpRequestData req,
+            FunctionContext executionContext)
+        {
+            ILogger log = executionContext.GetLogger(this.GetType().Name);
+
+            log.LogInformation($"Start RunListKakomonAsync: {req}");
+
+            // TODO 定義して読む
+            var body = new ListKakomonResponse(new List<ExamPartSummary>()
+            {
+                new ExamPartSummary("2021r03a_ap", "2021r03a_ap_am_qs"),
+            });
+            var res = req.CreateResponse(HttpStatusCode.OK);
+            res.Headers.Add("Content-Type", "application/json");
+            res.WriteString(JsonConvert.SerializeObject(body));
+            return res;
+        }
+
+        //public record NextNewKakomonRequest(string ExamId, string ExamPartId) { }
+        public record NextNewKakomonResponse(QuestionSummary Question) { }
+
+        [Function("NextKakomon")]
+        public async Task<HttpResponseData> RunNextNewKakomonAsync(
+            [HttpTrigger(AuthorizationLevel.Function, "get")] HttpRequestData req,
+            FunctionContext executionContext)
+        {
+            ILogger log = executionContext.GetLogger(this.GetType().Name);
+
+            log.LogInformation($"Start RunNextNewKakomonAsync: {req}");
+
+            var query = QueryHelpers.ParseQuery(req.Url.Query);
+            var examId = query["ExamId"].First();
+            var examPartId = query["ExamPartId"].First();
+
+            var examPart = await _repository.GetExamPartAsync(examId, examPartId);
+            var examPartOnTwitter = await _repository.GetExamPartOnTwitterAsync(examId, examPartId);
+
+            var tweetedQuestionNos = examPartOnTwitter.Questions.Select(x => x.No).ToHashSet();
+            var nonTweetQuestions = examPart.Questions.Where(x => !tweetedQuestionNos.Contains(x.No)).ToList();
+            var selectedQuestion = nonTweetQuestions.Any() ? nonTweetQuestions.ChoseRandom() : examPart.Questions.ChoseRandom();
+
+            var body = new NextNewKakomonResponse(
+                new QuestionSummary(examPart.ExamId, examPart.ExamPartId, selectedQuestion.No)
+            );
+            var res = req.CreateResponse(HttpStatusCode.OK);
+            res.Headers.Add("Content-Type", "application/json");
+            res.WriteString(JsonConvert.SerializeObject(body));
+            return res;
         }
 
         public record TweetNewKakomonRequest(string ExamId, string ExamPartId, int QuestionNo) { }
