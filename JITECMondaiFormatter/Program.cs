a144b83f0bb578a9785232150291f7bfc78ac0d6
koudenpa <koudenpa@hotmail.com>
2022-04-10 00:17:12 +0900

    Tesseract試し
    精度と速度が出なさそうなので保留。学習データはコミットしていない。

--- JITECMondaiFormatter/Program.cs
+++ JITECMondaiFormatter/Program.cs
@@ -11,12 +11,9 @@ using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
-using Windows.Globalization;
-using Windows.Graphics.Imaging;
-using Windows.Media.Ocr;
-using Windows.Storage.Streams;
 using SixLabors.Fonts;
 using SixLabors.ImageSharp.Drawing.Processing;
+using Tesseract;
 
 namespace JITECMondaiFormatter
 {
@@ -55,12 +52,19 @@ namespace JITECMondaiFormatter
             await File.WriteAllTextAsync(outputFilePath, JsonConvert.SerializeObject(output));
         }
 
+        public record OcrLine(
+            string Text,
+            Rect Bounds
+            )
+        { }
+
         public static async Task<IEnumerable<Question>> ReadQuestion(
             InputItem inputItem, string outputDirPath)
         {
             var pngEnc = new PngEncoder();
             var rawEnc = new BmpEncoder();
-            var ocr = OcrEngine.TryCreateFromLanguage(new Language("ja"));
+            // https://github.com/tesseract-ocr/tessdata
+            using var ocr = new TesseractEngine(@"./tessdata", "eng+jpn", EngineMode.Default);
 
             using var doc = new PdfDocument(inputItem.QuestionFilePath);
             var pageNumber = 0;
@@ -88,96 +92,120 @@ namespace JITECMondaiFormatter
                 await rawPageImage.SaveAsync(pageImagePath, pngEnc);
 
                 // OCR
-                using var bmpStream = new InMemoryRandomAccessStream();
-                await normalizedPageImage.SaveAsync(bmpStream.AsStream(), pngEnc);
-                var bmpDec = await BitmapDecoder.CreateAsync(bmpStream);
-                var ocrRes = await ocr.RecognizeAsync(await bmpDec.GetSoftwareBitmapAsync());
-                await File.WriteAllTextAsync(pageTextPath, ocrRes.Text);
+                using var bmpStream = new MemoryStream();
+                await normalizedPageImage.SaveAsync(bmpStream, pngEnc);
+                bmpStream.Position = 0;
+                using var ocrImage = Pix.LoadFromMemory(bmpStream.ToArray());
+                using var ocrRes = ocr.Process(ocrImage);
+                await File.WriteAllTextAsync(pageTextPath, ocrRes.GetText());
 
-                // Detect Question
-                var qLines = new List<OcrLine>();
-                int detectedQNo = -1;
-                foreach (var ocrLine in ocrRes.Lines)
+                var lines = new List<OcrLine>();
+                using var ite = ocrRes.GetIterator();
+                do
                 {
-                    // XXX テキストが斜めってるおかげで「問」より先に次の設問の本文が来てしまう
-                    var isNewQ = ocrLine.Text.StartsWith("問");
-                    if (isNewQ)
-                    {
-                        var q = await WriteQuestion(inputItem, pngEnc, qNo, detectedQNo, normalizedPageImage, qLines, outputDirPath);
-                        if (q != null) { qList.Add(q); }
-                        qLines.Clear();
-                        int.TryParse(ocrLine.Words.Skip(1).FirstOrDefault()?.Text, out detectedQNo);
-                        // XXX 80だけ89になってしまった
-                        if (detectedQNo > 0)
-                        {
-                            qNo++;
-                        }
-                    }
-                    qLines.Add(ocrLine);
+                    var text = ite.GetText(PageIteratorLevel.TextLine);
+                    Rect bounds;
+                    ite.TryGetBoundingBox(PageIteratorLevel.TextLine, out bounds);
+                    lines.Add(new OcrLine(text, bounds));
                 }
-                {
-                    var q = await WriteQuestion(inputItem, pngEnc, qNo, detectedQNo, normalizedPageImage, qLines, outputDirPath);
-                    if (q != null) { qList.Add(q); }
-                }
+                while (ite.Next(PageIteratorLevel.TextLine));
+
+                //// Detect Question
+                //var qLines = new List<OcrLine>();
+                //int detectedQNo = -1;
+                //foreach (var ocrLine in lines)
+                //{
+                //    // XXX テキストが斜めってるおかげで「問」より先に次の設問の本文が来てしまう
+                //    var isNewQ = ocrLine.Text.StartsWith("問");
+                //    if (isNewQ)
+                //    {
+                //        var q = await WriteQuestion(inputItem, pngEnc, qNo, detectedQNo, normalizedPageImage, qLines, outputDirPath);
+                //        if (q != null) { qList.Add(q); }
+                //        qLines.Clear();
+                //        int.TryParse(ocrLine.Words.Skip(1).FirstOrDefault()?.Text, out detectedQNo);
+                //        if (detectedQNo > 0)
+                //        {
+                //            qNo++;
+                //        }
+                //    }
+                //    qLines.Add(ocrLine);
+                //}
+                //{
+                //    var q = await WriteQuestion(inputItem, pngEnc, qNo, detectedQNo, normalizedPageImage, qLines, outputDirPath);
+                //    if (q != null) { qList.Add(q); }
+                //}
             }
 
             return qList;
         }
 
-        private static async Task<Image> NormalizePageImage(OcrEngine ocr, Image pageImage)
+        private static async Task<Image> NormalizePageImage(TesseractEngine ocr, Image pageImage)
         {
-            using var normalizeStream = new InMemoryRandomAccessStream();
-            await pageImage.SaveAsBmpAsync(normalizeStream.AsStream());
-            var normalizeDec = await BitmapDecoder.CreateAsync(normalizeStream);
-            var normalizeRes = await ocr.RecognizeAsync(await normalizeDec.GetSoftwareBitmapAsync());
-            // XXX TextAngle = 0 で傾きは得られていない
-            Console.WriteLine(normalizeRes.Text);
-            Console.WriteLine(normalizeRes.TextAngle);
-            return pageImage.Clone(x => x.Rotate((float)(normalizeRes.TextAngle ?? 0d)));
-        }
+            using var bmpStream = new MemoryStream();
+            await pageImage.SaveAsBmpAsync(bmpStream);
+            bmpStream.Position = 0;
+            using var ocrImage = Pix.LoadFromMemory(bmpStream.ToArray());
+            using var ocrRes = ocr.Process(ocrImage);
 
-        // TODO パラメータ化など
-        private static FontCollection collection = new();
-        private static FontFamily family = collection.Add("Font/ipaexg.ttf");
-        private static Font font = family.CreateFont(14, FontStyle.Italic);
-        private static async Task<Question> WriteQuestion(
-            InputItem inputItem,
-            PngEncoder enc, int qNo, int detectedQNo, Image pageImage, IEnumerable<OcrLine> qLines, string outputDirPath)
-        {
-            if (!qLines.Any() || detectedQNo < 1)
+            int orientation;
+            float confidence;
+            ocrRes.DetectBestOrientation(out orientation, out confidence);
+            Console.WriteLine($"orientation:{orientation}, confidence:{confidence}");
+
+            using var ite = ocrRes.GetIterator();
+            while (ite.Next(PageIteratorLevel.TextLine))
             {
-                return null;
+                Console.WriteLine(ite.GetText(PageIteratorLevel.TextLine));
+                Rect bounds;
+                ite.TryGetBoundingBox(PageIteratorLevel.TextLine, out bounds);
+                Console.WriteLine(bounds);
             }
 
-            var qImagePath = Path.Combine(outputDirPath, $"q{qNo.ToString("000")}.png");
-            var qTextPath = Path.Combine(outputDirPath, $"q{qNo.ToString("000")}.txt");
-
-            // ページ番号行を消し飛ばす
-            var pageNoReg = new Regex(@"^[ -~]+$");
-            var normalizedLines = qLines.Reverse().SkipWhile(x => pageNoReg.IsMatch(x.Text)).Reverse().ToList();
-
-            // XXX 幅ビミョーなので固定しておく
-            //var qLeft = (int)Math.Max(0, normalizedLines.Min(x => x.Words.Min(y => y.BoundingRect.Left)) - 8);
-            //var qRight = (int)Math.Min(pageImage.Width, normalizedLines.Max(x => x.Words.Max(y => y.BoundingRect.Left)) + 8);
-            var qLeft = (int)(pageImage.Width * 0.05);
-            var qRight = (int)(pageImage.Width * 0.95);
-            var qTop = (int)Math.Max(0, normalizedLines.Min(x => x.Words.Min(y => y.BoundingRect.Top)) - 16);
-            var qBottom = (int)Math.Min(pageImage.Height, normalizedLines.Max(x => x.Words.Max(y => y.BoundingRect.Top)) + 64);
-            var refText = $"出典: {inputItem.ExamRefName} 問 {string.Format("{0,2}", qNo)}";
-
-            using var qImage = pageImage
-                .Clone(x => x.Crop(new Rectangle(qLeft, qTop, qRight - qLeft, qBottom - qTop)));
-            // TODO 右寄せ面倒臭いけれど右寄せのほうがよさそう
-            qImage.Mutate(x => x.DrawText(refText, font, Color.Black, new PointF(8, qImage.Height - 24)));
-            await qImage.SaveAsync(qImagePath, enc);
-
-            var qText = string.Join(Environment.NewLine, normalizedLines.Select(x => x.Text));
-            //await File.WriteAllTextAsync(qTextPath, qText);
-            // Log
-            Console.WriteLine(qText);
-
-            // XXX 回答ここで処理できるわけねーだろ
-            return new Question(qNo, qText, qImagePath.Replace("\\", "/"), "-");
+            return pageImage.Clone(x => { });// (x => x.Rotate((float)(normalizeRes.TextAngle ?? 0d)));
         }
+
+        //// TODO パラメータ化など
+        //private static FontCollection collection = new();
+        //private static FontFamily family = collection.Add("Font/ipaexg.ttf");
+        //private static Font font = family.CreateFont(14, FontStyle.Italic);
+        //private static async Task<Question> WriteQuestion(
+        //    InputItem inputItem,
+        //    PngEncoder enc, int qNo, int detectedQNo, Image pageImage, IEnumerable<OcrLine> qLines, string outputDirPath)
+        //{
+        //    if (!qLines.Any() || detectedQNo < 1)
+        //    {
+        //        return null;
+        //    }
+
+        //    var qImagePath = Path.Combine(outputDirPath, $"q{qNo.ToString("000")}.png");
+        //    var qTextPath = Path.Combine(outputDirPath, $"q{qNo.ToString("000")}.txt");
+
+        //    // ページ番号行を消し飛ばす
+        //    var pageNoReg = new Regex(@"^[ -~]+$");
+        //    var normalizedLines = qLines.Reverse().SkipWhile(x => pageNoReg.IsMatch(x.Text)).Reverse().ToList();
+
+        //    // XXX 幅ビミョーなので固定しておく
+        //    //var qLeft = (int)Math.Max(0, normalizedLines.Min(x => x.Words.Min(y => y.BoundingRect.Left)) - 8);
+        //    //var qRight = (int)Math.Min(pageImage.Width, normalizedLines.Max(x => x.Words.Max(y => y.BoundingRect.Left)) + 8);
+        //    var qLeft = (int)(pageImage.Width * 0.05);
+        //    var qRight = (int)(pageImage.Width * 0.95);
+        //    var qTop = (int)Math.Max(0, normalizedLines.Min(x => x.Words.Min(y => y.BoundingRect.Top)) - 16);
+        //    var qBottom = (int)Math.Min(pageImage.Height, normalizedLines.Max(x => x.Words.Max(y => y.BoundingRect.Top)) + 64);
+        //    var refText = $"出典: {inputItem.ExamRefName} 問 {string.Format("{0,2}", qNo)}";
+
+        //    using var qImage = pageImage
+        //        .Clone(x => x.Crop(new Rectangle(qLeft, qTop, qRight - qLeft, qBottom - qTop)));
+        //    // TODO 右寄せ面倒臭いけれど右寄せのほうがよさそう
+        //    qImage.Mutate(x => x.DrawText(refText, font, Color.Black, new PointF(8, qImage.Height - 24)));
+        //    await qImage.SaveAsync(qImagePath, enc);
+
+        //    var qText = string.Join(Environment.NewLine, normalizedLines.Select(x => x.Text));
+        //    //await File.WriteAllTextAsync(qTextPath, qText);
+        //    // Log
+        //    Console.WriteLine(qText);
+
+        //    // XXX 回答ここで処理できるわけねーだろ
+        //    return new Question(qNo, qText, qImagePath.Replace("\\", "/"), "-");
+        //}
     }
 }
